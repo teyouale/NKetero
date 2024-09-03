fbc46ab042942b9bcd0d7e50e8eeb8c7e91de604
teyouale <teyouale@gmail.com>
2024-09-03 06:47:31 +0300

    Adding Frontend Workspace

---
+++ apps/frontend/src/app/app.tsx
@@ -0,0 +1,12 @@
+import { Route, Routes, Link } from 'react-router-dom';
+
+export function App() {
+  return (
+    <div>
+      <h1 className="bg-primary">Hello</h1>
+      {/* <NxWelcome title="frontend" /> */}
+    </div>
+  );
+}
+
+export default App;
